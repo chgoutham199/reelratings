d292c2e766251461483c03e7340a89a2a1ff0aa7
chgoutham199 <chgoutham199@gmail.com>
2024-04-13 16:42:25 +0530

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,29 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 gap-4 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        In in ligula ut ex malesuada tempor. Vivamus a commodo sapien, sit amet
+        fermentum velit. Morbi consectetur turpis sit amet mauris fringilla, non
+        maximus sem mattis. Interdum et malesuada fames ac ante ipsum primis in
+        faucibus. Nunc luctus nulla non enim aliquam auctor. Morbi vitae
+        hendrerit odio, vitae convallis erat. Aliquam erat volutpat. Vestibulum
+        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+        curae; Orci varius natoque penatibus et magnis dis parturient montes,
+        nascetur ridiculus mus. Morbi porttitor commodo odio, vel condimentum
+        nisl tempor tristique. Donec auctor eget purus vitae eleifend.
+      </p>
+      <p>
+        In in ligula ut ex malesuada tempor. Vivamus a commodo sapien, sit amet
+        fermentum velit. Morbi consectetur turpis sit amet mauris fringilla, non
+        maximus sem mattis. Interdum et malesuada fames ac ante ipsum primis in
+        faucibus. Nunc luctus nulla non enim aliquam auctor. Morbi vitae
+        hendrerit odio, vitae convallis erat. Aliquam erat volutpat. Vestibulum
+        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+        curae; Orci varius natoque penatibus et magnis dis parturient montes,
+        nascetur ridiculus mus. Morbi porttitor commodo odio, vel condimentum
+        nisl tempor tristique. Donec auctor eget purus vitae eleifend.
+      </p>
+    </div>
+  );
+}
