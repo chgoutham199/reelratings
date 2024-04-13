3113341f0da9d18907f05e36da1c659e39560f30
chgoutham199 <chgoutham199@gmail.com>
2024-04-13 15:55:40 +0530

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+import Menuitem from "./Menuitem";
+import {AiFillHome} from "react-icons/ai";
+import {BsFillInfoCircleFill} from "react-icons/bs";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+        <div className="flex gap-4 ">
+            <Menuitem title="Home" address="/" Icon={AiFillHome} />
+            <Menuitem title="about" address="/about" Icon={BsFillInfoCircleFill} />
+        </div>
+
+        <Link href="/" className="flex gap-1 items-center">
+             <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">Reel</span>
+             <span className="text-xl hidden sm:inline">Ratings</span>
+        </Link>
+        
+        
+    </div>
+  )
+}
