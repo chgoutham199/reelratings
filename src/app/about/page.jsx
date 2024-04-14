473d1976a310d0584f985a1b13d212215ece49d6
chgoutham199 <chgoutham199@gmail.com>
2024-04-14 22:00:49 +0530

    add about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,26 +3,26 @@ export default function About() {
     <div className="max-w-6xl mx-auto p-3 gap-4 space-y-4">
       <h1 className="text-2xl font-medium text-amber-600">About</h1>
       <p>
-        In in ligula ut ex malesuada tempor. Vivamus a commodo sapien, sit amet
-        fermentum velit. Morbi consectetur turpis sit amet mauris fringilla, non
-        maximus sem mattis. Interdum et malesuada fames ac ante ipsum primis in
-        faucibus. Nunc luctus nulla non enim aliquam auctor. Morbi vitae
-        hendrerit odio, vitae convallis erat. Aliquam erat volutpat. Vestibulum
-        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-        curae; Orci varius natoque penatibus et magnis dis parturient montes,
-        nascetur ridiculus mus. Morbi porttitor commodo odio, vel condimentum
-        nisl tempor tristique. Donec auctor eget purus vitae eleifend.
+        Our project aims to create a dynamic and user-friendly website dedicated
+        to showcasing a comprehensive list of movies sourced from the TMDB (The
+        Movie Database) API. The website will cater to movie enthusiasts by
+        offering sections dedicated to trending, upcoming, and popular movies,
+        providing users with easy access to the latest and most sought-after
+        cinematic releases. Leveraging the vast database provided by TMDB, our
+        website will ensure that visitors stay informed about the hottest movies
+        in the industry.
       </p>
       <p>
-        In in ligula ut ex malesuada tempor. Vivamus a commodo sapien, sit amet
-        fermentum velit. Morbi consectetur turpis sit amet mauris fringilla, non
-        maximus sem mattis. Interdum et malesuada fames ac ante ipsum primis in
-        faucibus. Nunc luctus nulla non enim aliquam auctor. Morbi vitae
-        hendrerit odio, vitae convallis erat. Aliquam erat volutpat. Vestibulum
-        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-        curae; Orci varius natoque penatibus et magnis dis parturient montes,
-        nascetur ridiculus mus. Morbi porttitor commodo odio, vel condimentum
-        nisl tempor tristique. Donec auctor eget purus vitae eleifend.
+        Through seamless integration with the TMDB API, our website will
+        dynamically fetch and update movie listings, ensuring that the content
+        remains fresh and relevant. The trending section will highlight movies
+        gaining traction in real-time, offering users insight into what's
+        currently captivating audiences worldwide. Meanwhile, the upcoming
+        section will provide a glimpse into the future of cinema, showcasing
+        movies slated for release in the near future. Additionally, the popular
+        section will feature films that have garnered significant attention and
+        acclaim, allowing users to explore titles that have captured the hearts
+        and minds of viewers globally
       </p>
     </div>
   );
