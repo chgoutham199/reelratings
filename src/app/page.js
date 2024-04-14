77d6dcc9cdf1a5001c9c21345ca786c5af577e68
chgoutham199 <chgoutham199@gmail.com>
2024-04-14 18:28:34 +0530

    feat : fetch tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
-import Image from 'next/image'
+import Results from "@/components/Results";
 
-export default function Home() {
+const API_KEY=process.env.API_KEY
+export default async function Home({searchParams}) {
+    const genre=searchParams.genre || 'now_playing'
+    console.log(genre);
+           const res= await fetch(`https://api.themoviedb.org/3/movie/${genre}?api_key=${API_KEY}&language=en-US&page=1`)
+          const data = await res.json();
+          console.log(data);
+        if(!res.ok) 
+        {
+          throw new Error('Failed to fetch data')
+        }
+       const results=data.results
   return (
        
     <div>
-       Hello
+     <Results results={results} />
     </div>
        
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,10 @@ import NavBarItem from "./NavBarItem";
 export default function NavBar() {
   return (
     <div className='flex dark:bg-gray-600 bg-amber-200 p-4 lg:text-lg justify-center gap-6 '>
-        <NavBarItem title="Trending" param="fetchTrending" />
-        <NavBarItem title="Popular" param="fetchPopular" />
-        <NavBarItem title="Top Rated" param="fetchTopRated" />
-        <NavBarItem title="Upcoming" param="fetchUpcoming" />
+        <NavBarItem title="Trending" param="now_playing" />
+        <NavBarItem title="Popular" param="popular" />
+        <NavBarItem title="Top Rated" param="top_rated" />
+        <NavBarItem title="Upcoming" param="upcoming" />
    
     </div>
   )
