b4f6e94fca2277f6107f310d42b802e74dc1cc6a
chgoutham199 <chgoutham199@gmail.com>
2024-04-14 19:01:59 +0530

    add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+export default function loading() {
+  return (
+    <div className="flex justify-center">
+         <img className="h-52 " src="spinner.svg" alt="loading...." />
+
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ const API_KEY=process.env.API_KEY
 export default async function Home({searchParams}) {
     const genre=searchParams.genre || 'now_playing'
     console.log(genre);
-           const res= await fetch(`https://api.themoviedb.org/3/movie/${genre}?api_key=${API_KEY}&language=en-US&page=1`)
+           const res= await fetch(`https://api.themoviedb.org/3/movie/${genre}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:10000}})
           const data = await res.json();
           console.log(data);
         if(!res.ok) 
