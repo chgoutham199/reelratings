3b02383431f0ae4b6a14460b2af73935d708f613
chgoutham199 <chgoutham199@gmail.com>
2024-04-14 20:38:13 +0530

    fix error

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ export default function Card({result}) {
   return (
     <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
       <Link href={`/movie/${result.id}`} >
-        <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`} width={500} height={300} className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-300 " >
+        <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`} alt={result.title ||result.name} width={500} height={300} className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-300 " >
         </Image>
        <div className="">
           <h2 className="text-lg font-bold truncate">{result.title ||result.name}</h2>
